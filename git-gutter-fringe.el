608fd06ec5a91bd3ba3ad3694f9b9e427170320e
Syohei YOSHIDA <syohex@gmail.com>
2013-03-07 16:05:05 +0900

    Update requirement package version

--- git-gutter-fringe.el
+++ git-gutter-fringe.el
@@ -4,8 +4,8 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-git-gutter-fringe
-;; Version: 0.05
-;; Package-Requires: ((git-gutter "0.24") (fringe-helper "0.1.1"))
+;; Version: 0.06
+;; Package-Requires: ((git-gutter "0.26") (fringe-helper "0.1.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

521a43bb6b495d367a720941c226e7d717285862
Syohei YOSHIDA <syohex@gmail.com>
2013-11-17 23:35:45 +0900

    Add comment section

--- git-gutter-fringe.el
+++ git-gutter-fringe.el
@@ -22,6 +22,18 @@
 
 ;;; Commentary:
 
+;; Show git diff information in fringe. You can use this package
+;; only GUI Emacs, not working no window emacs.
+
+;; To use this package, add following code to your init.el or .emacs
+;;
+;; (require 'git-gutter-fringe)
+;; (global-git-gutter-mode t)
+;;
+;; ; If you want to show git diff information at right fringe
+;; (setq git-gutter-fr:side 'right-fringe)
+;;
+
 ;;; Code:
 
 (eval-when-compile

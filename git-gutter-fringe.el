a75a8ac994889591c77d61e4bcaeaf0c3ce288e9
Syohei YOSHIDA <syohex@gmail.com>
2014-07-31 14:58:36 +0900

    Bump up version and update Changes

--- git-gutter-fringe.el
+++ git-gutter-fringe.el
@@ -4,8 +4,8 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-git-gutter-fringe
-;; Version: 0.18
-;; Package-Requires: ((git-gutter "0.67") (fringe-helper "0.1.1") (cl-lib "0.5") (emacs "24"))
+;; Version: 0.19
+;; Package-Requires: ((git-gutter "0.71") (fringe-helper "0.1.1") (cl-lib "0.5") (emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
